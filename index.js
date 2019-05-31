7f7e88d45732c44836a52893696ea47c962fec04
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-31 12:37:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.push(name)} in line.`;
 }
 
 
